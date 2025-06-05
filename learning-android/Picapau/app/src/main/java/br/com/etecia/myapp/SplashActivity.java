4300bca5d92ab26e9e21bf6781d5972e8baea322
Rafalvs <rafaelalvessantana0401@gmail.com>
2025-06-05 20:03:28 -0300

    ANDRTOUD

---
+++ learning-android/Picapau/app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package br.com.etecia.myapp;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    // Set the duration of the splash screen in milliseconds
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        // Use a Handler to post a delayed action for transitioning to the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after the splash screen duration
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
