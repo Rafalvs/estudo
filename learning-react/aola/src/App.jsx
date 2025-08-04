c1e24ee182787a02ecef33a993dc9aa5317e8630
rafalvs <rafaelalvessantana0401@gmail.com>
2025-08-04 20:13:35 -0300

    firebase pojeto

---
+++ learning-react/aola/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+    <p>pipipipopopo</p>
+    <h1>guilherme paiaço</h1>
+    <h2>PNEMONIA</h2>
+    <p>saguadinho</p>
+    <p>xarxixa</p>
+    <p>quejunto e preijo</p>
+    <p>embigo</p>
+    <p>anieligena</p>
+    <h3>ensonia</h3>
+    <h4>probrema</h4>
+    </>    
+  )
+}
